f95502c3d34b56cdfe4005859ddcb55bec163ce7
enriquefnt <enriquefinetti@gmail.com>
2023-10-07 14:55:25 -0300

    nada

---
+++ public/conecta.php
@@ -0,0 +1,4 @@
+<?php
+$pdoO = new PDO('mysql:host=200.45.111.99;dbname=MSP_NUTRICION; charset=utf8', 'SiViNSalta', '@#sivin#@salta!%2020&&');
+$pdoD = new PDO('mysql:host=212.1.210.73;dbname=saltaped_sivinsalta; charset=utf8', 'saltaped_sivin', 'sivin7625');
+?>
